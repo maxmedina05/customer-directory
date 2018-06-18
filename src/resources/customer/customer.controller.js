0cb42046e82f4c2e167b36579b4f0b9f4e74fbed
Max Medina <maxmedina05@gmail.com>
2018-06-18 19:59:57 -0400

    Add update customer

--- src/resources/customer/customer.controller.js
+++ src/resources/customer/customer.controller.js
@@ -1,6 +1,36 @@
 const Customer = require("./customer.schema");
+const Agp = require("api-query-params");
 
-async function getAllcustomer(req, res) {}
+async function getAllcustomer(req, res) {
+  try {
+    const {
+      filter,
+      skip = 0,
+      limit = 20,
+      sort,
+      projection = { __v: 0, createdAt: 0, modifiedAt: 0 }
+    } = Agp(req.query);
+    const total = await Customer.count(filter);
+    const customers = await Customer.find(filter)
+      .find(filter)
+      .skip(skip)
+      .limit(limit)
+      .select(projection)
+      .sort(sort);
+
+    res.set("X-Total-Count", total);
+    res.json({
+      payload: customers,
+      count: customers.length,
+      total
+    });
+  } catch (err) {
+    res.status(400).json({
+      payload: null,
+      error: err.message || err
+    });
+  }
+}
 
 async function addCustomer(req, res) {
   const {
@@ -12,7 +42,7 @@ async function addCustomer(req, res) {
   } = req.body;
 
   try {
-    const newCustomer = new Customer({
+    const newCustomer = await Customer.create({
       name,
       birthday,
       gender,
@@ -20,9 +50,10 @@ async function addCustomer(req, res) {
       customerLifetimeValue
     });
 
-    await newCustomer.save();
-
-    res.status(201).json(newCustomer);
+    res.status(201).json({
+      payload: newCustomer,
+      error: false
+    });
   } catch (err) {
     res.status(400).json({
       payload: null,
@@ -31,7 +62,68 @@ async function addCustomer(req, res) {
   }
 }
 
+async function getCustomer(req, res) {
+  const { customerId } = req.params;
+  try {
+    const customer = await Customer.findOne(
+      { customerId },
+      { __v: 0, createdAt: 0, modifiedAt: 0 }
+    );
+
+    res.json({ payload: customer });
+  } catch (err) {
+    res.status(400).json({
+      payload: null,
+      error: err.message || err
+    });
+  }
+}
+
+async function updateCustomer(req, res) {
+  const { customerId } = req.params;
+  const {
+    name,
+    birthday,
+    gender,
+    lastContact,
+    customerLifetimeValue
+  } = req.body;
+
+  try {
+    const customer = await Customer.findOne({ customerId });
+    customer.name = name ? name : customer.name;
+    customer.birthday = birthday ? birthday : customer.birthday;
+    customer.gender = gender ? gender : customer.gender;
+    customer.lastContact = lastContact ? lastContact : customer.lastContact;
+    customer.customerLifetimeValue = customerLifetimeValue
+      ? customerLifetimeValue
+      : customer.customerLifetimeValue;
+
+    res.json({ payload: customer });
+  } catch (err) {
+    res.status(400).json({
+      payload: null,
+      error: err.message || err
+    });
+  }
+}
+
+async function deleteCustomer(req, res) {
+  const customerId = req.params.customerId;
+  try {
+    const deletedCustomer = await Customer.deleteOne({
+      customerId
+    });
+    res.status(204).send();
+  } catch (err) {
+    res.status(400).json({ payload: null, error: err });
+  }
+}
+
 module.exports = {
   getAllcustomer,
-  addCustomer
+  addCustomer,
+  deleteCustomer,
+  getCustomer,
+  updateCustomer
 };
--- src/resources/customer/customer.router.js
+++ src/resources/customer/customer.router.js
@@ -1,8 +1,148 @@
 const express = require("express");
 const router = express.Router();
-const { getAllcustomer, addCustomer } = require("./customer.controller");
+const {
+  getAllcustomer,
+  addCustomer,
+  deleteCustomer,
+  getCustomer,
+  updateCustomer
+} = require("./customer.controller");
 
+/**
+ * @swagger
+ * /customers:
+ *   get:
+ *     tags:
+ *       - Customers
+ *     summary: Get all customers
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *      - name: offset
+ *        in: query
+ *        description: Indicates how many customers to skip. (Use this to handle pagination)
+ *        default: 0
+ *      - name: page
+ *        in: query
+ *        description: Indicates the current page to return. (Use this to handle pagination)
+ *        default: 0
+ *      - name: limit
+ *        in: query
+ *        description: Indicates how many customers to return.
+ *        default: 20
+ *      - name: fields
+ *        in: query
+ *        description: Indicates the customer's fields to be return in each object. Use - prefixes to return all fields except some specific fields.
+ *        default: ''
+ *      - name: sort
+ *        in: query
+ *        description: Indicates the fields to sort in ascending order. Use - prefixes to sort in descending order.
+ *        default: ''
+ *     responses:
+ *       200:
+ *         description: Return an object containing an array of customers
+ *       400:
+ *         description: Error
+ */
 router.get("/", getAllcustomer);
+
+/**
+ * @swagger
+ * /customers:
+ *   post:
+ *     tags:
+ *       - Customers
+ *     summary: Add a new customer
+ *     consumes:
+ *       - application/json
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *      - name: customer
+ *        in: body
+ *        required: true
+ *        schema:
+ *         $ref: '#/definitions/Customer'
+ *     responses:
+ *       201:
+ *         description: successful operation
+ *       400:
+ *         description: Error
+ */
 router.post("/", addCustomer);
+
+/**
+ * @swagger
+ * /customers/{customerId}:
+ *   delete:
+ *     tags:
+ *       - Customers
+ *     summary: Deletes a customer
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *      - name: number
+ *        in: param
+ *        description: The Id of the customer to delete.
+ *        required: true
+ *     responses:
+ *       200:
+ *         description: Return an object containing the deleted customer
+ *       400:
+ *         description: Error
+ */
+router.delete("/:customerId", deleteCustomer);
+
+/**
+ * @swagger
+ * /customers/{customerId}:
+ *   get:
+ *     tags:
+ *       - Customers
+ *     summary: Get a customer by its customerId.
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *      - name: customerId
+ *        in: param
+ *        description: The customerId of customer that needs to be found.
+ *        required: true
+ *     responses:
+ *       200:
+ *         description: Return an object containing the customer
+ *       400:
+ *         description: Error
+ */
+
+router.get("/:customerId", getCustomer);
+
+/**
+ * @swagger
+ * /customers/{customerId}:
+ *   put:
+ *     tags:
+ *       - Customers
+ *     summary: Updates an existing customer
+ *     consumes:
+ *       - application/json
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *      - name: customerId
+ *        in: param
+ *        description: The customerId of customer that needs to update.
+ *        required: true
+ *      - name: customer
+ *        in: body
+ *        required: true
+ *        schema:
+ *         $ref: '#/definitions/Customer'
+ *     responses:
+ *       200:
+ *         description: Return an object containing the updated customer
+ *       400:
+ *         description: Error
+ */
+router.put("/:customerId", updateCustomer);
 
 module.exports = router;
