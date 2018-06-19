269f1d2c9dfbf54bf96f86e87389b73101878c03
Max Medina <maxmedina05@gmail.com>
2018-06-19 11:46:42 -0400

    deploy to heroku

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const app = require("express")();
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
+const express = require('express');
+const path = require('path');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-const BASE_API_URL = "/api/v1";
-const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost/customerDB";
+const BASE_API_URL = '/api/v1';
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost/customerDB';
 const PORT = process.env.PORT || 3000;
 
-const resources = require("./src/resources");
+const resources = require('./src/resources');
 const customerResource = resources.customer;
 
 bodyParser.urlencoded({ extended: true });
@@ -15,6 +17,15 @@ mongoose.connect(MONGO_URI);
 
 app.use(`${BASE_API_URL}/customers`, customerResource.router);
 
+if (process.env.NODE_ENV === 'development') {
+  app.use(express.static('client/dist/client'));
+  app.get('*', (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, 'client', 'dist', 'client', 'index.html')
+    );
+  });
+}
+
 app.listen(PORT, () => {
-  console.log("App running on port:", PORT);
+  console.log('App running on port:', PORT);
 });
